8d0dba3dbbc6d9c05d3fb07c822b93b424f00d4b
hanep77 <yudishan26@gmail.com>
2024-11-05 11:11:27 +0700

    fix

--- src/app/quiz/page.tsx
+++ src/app/quiz/page.tsx
@@ -1,10 +1,10 @@
 export default function Quiz() {
-  const data: Array<any> = [
-    {
-      question: "pertanyaan pertama",
-      answers: ["jawaban 1", "jawaban 2", "jawaban 3", "jawaban 4"]
-    }
-  ]
+  // const data: Array<any> = [
+  //   {
+  //     question: "pertanyaan pertama",
+  //     answers: ["jawaban 1", "jawaban 2", "jawaban 3", "jawaban 4"]
+  //   }
+  // ]
   return (
     <div className="pt-20 bg-[rgba(28,3,50,1)] min-h-screen max-w-screen">
       <div className="max-w-screen-lg m-auto">
