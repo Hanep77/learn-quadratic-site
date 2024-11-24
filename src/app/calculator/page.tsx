c0912dbae450626dd12295c0eb0692798e3d295c
hanep77 <yudishan26@gmail.com>
2024-11-24 17:33:11 +0700

    hover

--- src/app/calculator/page.tsx
+++ src/app/calculator/page.tsx
@@ -123,8 +123,7 @@ export default function Calculator() {
               </div>
               <button
                 onClick={generateGraphData}
-                className="bg-green-500 hover:bg-green-600 active:bg-green-700 text-white px-3 py-2 rounded cursor-pointer z-10"
-              >
+                className="bg-green-600 hover:bg-green-700 active:bg-green-800 text-white px-3 py-2 rounded cursor-pointer z-10">
                 Buat Grafik
               </button>
             </div>
